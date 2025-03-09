55b82d619771218c5010851b13fc7c70afff1bfb
monumentum-dev <stronamonumentum@gmail.com>
2025-03-09 18:44:57 +0100

    feat: grid sistem

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -6,29 +6,31 @@ interface ChoiseProps {
   host: string; // Add the host prop here
 }
 
-const Choice: React.FC<ChoiseProps> = ({host}) => {
-  const [selectedOption, setSelectedOption] = useState<"files" | "link" | null>(null); 
+const Choice: React.FC<ChoiseProps> = ({ host }) => {
+  const [selectedOption, setSelectedOption] = useState<"files" | "link" | null>(null);
   return (
-    <div className="container text-center mt-5 ">
-      <div className="py-3 px-4">
-      {!selectedOption ? (
-        <div className="" >
-          <p className="text-uppercase text-center text-light mb-4 fs-5">Wybierz sposób przesyłania danych</p>
-          <p className="mb-3">Możesz przesłać pliki bezpośrednio lub podać link do Google Drive/Dropbox.</p>
-          <div className="d-flex justify-content-center gap-3">
-            <button className="btn btn-primary" onClick={() => setSelectedOption("files")}>📂 Prześlij pliki</button>
-            <button className="btn btn-secondary" onClick={() => setSelectedOption("link")}>🔗 Podaj link</button>
-          </div>
+    <div className="row">
+      <div className="col-12">
+        <div className="py-3 px-4">
+          {!selectedOption ? (
+            <div className="" >
+              <div className="text-uppercase text-center text-light fs-5">Wybierz sposób przesyłania danych</div>
+              <div className="mb-3 text-center">Możesz przesłać pliki bezpośrednio lub podać link do Google Drive/Dropbox.</div>
+              <div className="d-flex justify-content-center gap-3">
+                <button className="btn btn-primary" onClick={() => setSelectedOption("files")}>📂 Prześlij pliki</button>
+                <button className="btn btn-secondary" onClick={() => setSelectedOption("link")}>🔗 Podaj link</button>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <button className="btn btn-dark mb-3" onClick={() => setSelectedOption(null)}>
+                ⬅ Wróć do wyboru
+              </button>
+              {selectedOption === "files" ? <Form host={host} /> : <LinkForm host={host} />}
+            </div>
+          )}
         </div>
-      ) : (
-        <div>
-          <button className="btn btn-dark mb-3" onClick={() => setSelectedOption(null)}>
-            ⬅ Wróć do wyboru
-          </button>
-          {selectedOption === "files" ? <Form host={host}/> : <LinkForm host={host}/>}
-        </div>
-      )}
-    </div>
+      </div>
     </div>
   );
 };
